c6b12fa8f36daa2d9acfc16e3cdcf696d53ba3bf
shihab-newaz <shihab.newaz.5633@gmail.com>
2025-01-18 22:48:52 +0600

    / removed

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000', 
-      'https://www.shelterhousinglimited.com/', 
+      'https://www.shelterhousinglimited.com', 
     ],
     credentials: true,
   });
